11871c3c78e9cd0d3c9aab274dfc7132a5e7ab66
jeondain <dain.jeon03@gmail.com>
2025-09-08 20:50:49 +0900

    [Week00] 프롬/전다인 미션 제출

---
+++ week00/src/main/java/com/fromm/week00/Week00Application.java
@@ -0,0 +1,13 @@
+package com.fromm.week00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week00Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week00Application.class, args);
+    }
+
+}
---
+++ week00/src/test/java/com/fromm/week00/Week00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fromm.week00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week00ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
